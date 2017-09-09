d520d36557f61ae47f3eb5386d9d89cba12b622d
marthjod@gmail.com <marthjod@gmail.com>
2017-09-09 15:51:51 +0200

    refactor main.go to reflect current changes

--- main.go
+++ main.go
@@ -1,44 +1,69 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
 	"github.com/marthjod/bingo/gender"
+	"github.com/marthjod/bingo/getter"
 	"github.com/marthjod/bingo/wordtype"
 	"gopkg.in/xmlpath.v2"
+	"html"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
-	f, err := os.Open("orð.xml")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer f.Close()
 
-	root, err := xmlpath.Parse(f)
+	var (
+		query     = flag.String("q", "orð", "Word to query.")
+		urlPrefix = flag.String("url-prefix", "http://dev.phpbin.ja.is/ajax_leit.php", "Query URL prefix.")
+		debug     = flag.Bool("debug", false, "Enable debug output.")
+	)
+	flag.Parse()
+
+	g := getter.Getter{UrlPrefix: *urlPrefix}
+	resp, err := g.GetWord(*query)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	buf := bytes.NewBuffer(body)
+	escaped := html.UnescapeString(buf.String())
+
+	if *debug {
+		fmt.Println(escaped)
+	}
+
+	root, err := xmlpath.Parse(strings.NewReader(escaped))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// have we got a paradigm or a multiple-choice interstitial?
 
 	header := xmlpath.MustCompile("/div/h2")
 	if h, ok := header.String(root); ok {
 		wordType := wordtype.GetWordType(h)
 		fmt.Printf("Word type: %s\n", wordType)
-		if wordType == wordtype.Noun {
+		if wordType == wordtype.NounType {
 			fmt.Printf("Gender: %s\n", gender.GetGender(h))
-		}
-	}
 
-	path := xmlpath.MustCompile("//tr/td[2]")
-
-	iter := path.Iter(root)
-	for {
-		if !iter.Next() {
-			break
+			path := xmlpath.MustCompile("//tr/td[2]")
+			iter := path.Iter(root)
+			n := wordtype.ParseNoun(iter)
+			fmt.Printf("%s\n", n)
 		}
-		node := iter.Node()
-		fmt.Println(node.String())
 	}
 }
