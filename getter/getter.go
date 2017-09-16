c18b688115ae5e6b4327c70b703f4044a4c80e38
marthjod@gmail.com <marthjod@gmail.com>
2017-09-17 00:47:44 +0200

    make getter aware of multiple word choices

--- getter/getter.go
+++ getter/getter.go
@@ -1,14 +1,25 @@
 package getter
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/marthjod/bingo/reader"
+	"golang.org/x/net/html"
+	"gopkg.in/xmlpath.v2"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"regexp"
+	"strconv"
 )
 
 // Getter builds query URLs and HTTP requests against a data source.
 type Getter struct {
-	URLPrefix string
+	URLPrefix      string
+	ResponseBodies [][]byte
 }
 
 // WordQuery builds a URL for querying a word.
@@ -25,10 +36,117 @@ func (g *Getter) IDQuery(id int) (query string) {
 
 // GetWord makes an HTTP request for a word against the data source.
 func (g *Getter) GetWord(word string) (*http.Response, error) {
-	return http.Get(g.WordQuery(word))
+	query := g.WordQuery(word)
+	log.Debug("query: ", query)
+	r, err := http.Get(query)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	body := reader.Sanitize(b)
+
+	g.ResponseBodies = append(g.ResponseBodies, body)
+
+	addtlResponseData, err := g.Dispatch(body)
+	if err != nil {
+		return nil, err
+	}
+	for _, addtl := range addtlResponseData {
+		g.ResponseBodies = append(g.ResponseBodies, addtl)
+	}
+
+	return r, err
 }
 
 // GetID makes an HTTP request for a search ID against the data source.
 func (g *Getter) GetID(id int) (*http.Response, error) {
-	return http.Get(g.IDQuery(id))
+	query := g.IDQuery(id)
+	log.Debug("query: ", query)
+	return http.Get(query)
+}
+
+func (g *Getter) Dispatch(r []byte) (responseBodies [][]byte, err error) {
+	var reponseBodies = [][]byte{}
+
+	root, err := xmlpath.Parse(bytes.NewReader(r))
+	if err != nil {
+		return [][]byte{}, err
+	}
+	qLinks := xmlpath.MustCompile("/ul/li/strong/a")
+	if qLinks.Exists(root) {
+		doc, err := html.Parse(bytes.NewReader(r))
+		if err != nil {
+			return [][]byte{}, err
+		}
+
+		links := getLinkNodes(doc)
+		for _, link := range links {
+			id, err := getSearchID(link)
+			if err != nil {
+				return [][]byte{}, err
+			}
+			r, err := g.GetID(id)
+			if err != nil {
+				return [][]byte{}, err
+			}
+
+			body, err := readSanitized(r.Body)
+			if err != nil {
+				return [][]byte{}, err
+			}
+
+			reponseBodies = append(reponseBodies, body)
+		}
+	}
+
+	return reponseBodies, nil
+}
+
+func getSearchID(val string) (int, error) {
+	log.Debugf("obtaining search ID from %q", val)
+	searchID := regexp.MustCompile(`leit_id\('(\d+)'\)`)
+	groups := searchID.FindStringSubmatch(val)
+	if len(groups) > 1 {
+		return strconv.Atoi(groups[1])
+	}
+	return 0, errors.New("unable to convert search ID")
+}
+
+func getLinkNodes(doc *html.Node) []string {
+
+	var (
+		f     func(*html.Node)
+		links []string
+	)
+
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "onclick" {
+					links = append(links, a.Val)
+				}
+			}
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(doc)
+
+	return links
+}
+
+func readSanitized(r io.Reader) ([]byte, error) {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return []byte{}, err
+	}
+	return reader.Sanitize(b), nil
 }
